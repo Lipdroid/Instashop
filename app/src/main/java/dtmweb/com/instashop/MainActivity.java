cf89d3fb43ec139c063f9ee84962e80e9fa0d93d
lipuhossain <kutu1234>
2018-03-12 01:09:54 +0600

    drawer touch problem solved

--- app/src/main/java/dtmweb/com/instashop/MainActivity.java
+++ app/src/main/java/dtmweb/com/instashop/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private LinearLayout drawer_right_layout = null;
     private boolean isOutSideClicked = false;
 
+    private RelativeLayout main_container = null;
     //left drawers items
     private LinearLayout btn_manage_products = null;
 
@@ -50,7 +51,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void setListenersForViews() {
-        mDrawerLayout.setDrawerListener(mDrawerToggle);
         btn_left_drawer.setOnClickListener(this);
         btn_right_drawer.setOnClickListener(this);
         //left drawer listener
@@ -66,6 +66,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         drawer_left_layout = (RelativeLayout) findViewById(R.id.drawer_left);
         drawer_right_layout = (LinearLayout) findViewById(R.id.drawer_right);
 
+        main_container = (RelativeLayout) findViewById(R.id.main_container);
         //left drawer items
         btn_manage_products = (LinearLayout) findViewById(R.id.btn_manage_products);
     }
@@ -88,56 +89,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void afterClickRightDrawerBtn() {
         if (mDrawerLayout.isDrawerOpen(drawer_right_layout)) {
             mDrawerLayout.closeDrawer(drawer_right_layout);
+            //focus on the center layout
+            main_container.bringToFront();
+            main_container.requestLayout();
         } else if (!mDrawerLayout.isDrawerOpen(drawer_right_layout)) {
             mDrawerLayout.openDrawer(drawer_right_layout);
+            //focus on the right layout
+            drawer_right_layout.bringToFront();
+            drawer_right_layout.requestLayout();
         }
     }
 
     private void afterClickLeftDrawerBtn() {
         if (mDrawerLayout.isDrawerOpen(drawer_left_layout)) {
             mDrawerLayout.closeDrawer(drawer_left_layout);
+            //focus on the center layout
+            main_container.bringToFront();
+            main_container.requestLayout();
         } else if (!mDrawerLayout.isDrawerOpen(drawer_left_layout)) {
             mDrawerLayout.openDrawer(drawer_left_layout);
-        }
-    }
-
-    /*
-        Disable right drawer closing from inside touch
-     */
-
-    @Override
-    public boolean dispatchTouchEvent(MotionEvent event) {
-        if (event.getAction() == MotionEvent.ACTION_UP) {
-            if (mDrawerLayout.isDrawerOpen(drawer_right_layout)) { //Your code here to check whether drawer is open or not.
-
-                View content = findViewById(R.id.drawer_right); //drawer view id
-                int[] contentLocation = new int[2];
-                content.getLocationOnScreen(contentLocation);
-                Rect rect = new Rect(contentLocation[0],
-                        contentLocation[1],
-                        contentLocation[0] + content.getWidth(),
-                        contentLocation[1] + content.getHeight());
-
-                if ((rect.contains((int) event.getX(), (int) event.getY()))) {
-                    isOutSideClicked = true;
-                } else {
-                    isOutSideClicked = false;
-                }
-
-            } else {
-                return super.dispatchTouchEvent(event);
-            }
-        } else if (event.getAction() == MotionEvent.ACTION_DOWN && isOutSideClicked) {
-            isOutSideClicked = false;
-            return super.dispatchTouchEvent(event);
-        } else if (event.getAction() == MotionEvent.ACTION_MOVE && isOutSideClicked) {
-            return super.dispatchTouchEvent(event);
-        }
-
-        if (isOutSideClicked) {
-            return true; //restrict the touch event here
-        } else {
-            return super.dispatchTouchEvent(event);
+            //focus on the left layout
+            drawer_left_layout.bringToFront();
+            drawer_left_layout.requestLayout();
         }
     }
 
